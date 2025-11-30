7fd423199bc1819b7d0bb8969dacc90a494a1b18
Yvan De Souza <desouzayvan01@gmail.com>
2025-11-30 16:03:39 -0500

    Adding more complex queries

--- Queries.sql
+++ Queries.sql
@@ -56,3 +56,46 @@ LEFT JOIN fact_games f
 GROUP BY p.username
 ORDER BY total_games_played DESC
 LIMIT 10;
+
+
+-- White vs BLACK win rate
+SELECT 
+	SUM(CASE WHEN result = 'white_win' THEN 1 ELSE 0 END) AS white_win,
+	SUM(CASE WHEN result = 'black_win' THEN 1 ELSE 0 END) AS black_win,
+	SUM(CASE WHEN result = 'draw' THEN 1 ELSE 0 END) AS draws,
+	COUNT(*) AS total_games
+FROM fact_games;
+
+-- Number of games played in each year
+SELECT
+	d.year, 
+	COUNT(*) AS games_in_year
+FROM fact_games f
+JOIN dim_dates d ON f.date_id = d.date_id
+GROUP BY d.year;
+
+-- Average elo per time_control
+SELECT
+	t.category,
+	AVG((f.white_elo + f.black_elo)/2.0) AS avg_elo
+FROM fact_games f
+JOIN dim_time_controls t ON f.time_control_id = t.time_control_id
+GROUP BY t.category
+ORDER BY avg_elo DESC;
+
+-- Elo difference vs win rate
+SELECT
+	CASE 
+		WHEN (white_elo - black_elo) >= 200 THEN 'White +200'
+		WHEN (white_elo - black_elo) BETWEEN 50 AND 199 THEN 'White +50-199'
+		WHEN (white_elo - black_elo) BETWEEN -49 AND 49 THEN 'Even'
+		WHEN (white_elo - black_elo) BETWEEN -199 AND -50 THEN 'Black +50-199'
+		ELSE 'Black +200'
+	END AS rating_diff_bucket,
+	SUM(CASE WHEN result = 'white_win' THEN 1 ELSE 0 END) AS white_wins,
+	SUM(CASE WHEN result = 'black_win' THEN 1 ELSE 0 END) AS black_wins,
+	SUM(CASE WHEN result = 'draw' THEN 1 ELSE 0 END) AS draws,
+	COUNT(*) AS total_games
+FROM fact_games
+GROUP BY rating_diff_bucket
+ORDER BY total_games DESC;
